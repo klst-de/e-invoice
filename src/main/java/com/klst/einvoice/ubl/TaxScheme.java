2508ad906b6e3f0a5ff8f39df91e2cd9f80a0d10
homebeaver <homebeaver@outlook.de>
2020-12-21 22:26:35 +0100

    factory method to create different tax types

--- src/main/java/com/klst/einvoice/ubl/TaxScheme.java
+++ src/main/java/com/klst/einvoice/ubl/TaxScheme.java
@@ -4,11 +4,34 @@ import com.klst.untdid.codelist.TaxTypeCode;
 
 import oasis.names.specification.ubl.schema.xsd.commonaggregatecomponents_2.TaxSchemeType;
 
+/* Codeliste: UNTDID 5153 Eingeschränkt
+ * In der EN 16931 wird nur die Steuerart „Umsatzsteuer“ mit dem Code „VAT“ unterstützt.
+ * 
+ * Sollen andere Steuerarten angegeben wie 
+ * beispielsweise eine Versicherungssteuer oder eine Mineralölsteuer werden, 
+ * muss das EXTENDED Profil genutzt werden. 
+ * Der Code für die Steuerart muss dann der Codeliste UNTDID 5153 entnommen werden.
+ * https://service.unece.org/trade/untdid/d01b/trsd/trsdtax.htm
+ * 
+ * Geschäftsregel: BR-32 Abschläge auf Dokumentenebene
+ *  Jeder Abschlag auf Dokumentenebene (BG-20) muss einen Code für die für diesen Abschlag 
+ *  geltende Umsatzsteuer auf Dokumentenebene (BT-95) haben
+ */
 // verwendet in
 // ALLOWANCES (BG-20) and CHARGES (BG-21) : BT-95-0, BT-102-0
 // BG-23.BT-118-0
 public class TaxScheme extends TaxSchemeType {
 
+	/**
+	 * factory method to create different tax types
+	 * 
+	 * @param type enum, example TaxTypeCode.PetroleumTax or InsuranceTax
+	 * @return
+	 */
+	static public TaxScheme createTaxType(TaxTypeCode type) {
+		return new TaxScheme(type.getValue());
+	}
+	
 	TaxScheme(String code) {
 		super();
 		super.setID(new Identifier(code));
