ff946fdb5f7e871e6c984ffbef97a9690d8524c1
homebeaver <homebeaver@outlook.de>
2020-12-21 21:26:51 +0100

    Deprecated Identifier.newIDType

--- src/main/java/com/klst/einvoice/ubl/Identifier.java
+++ src/main/java/com/klst/einvoice/ubl/Identifier.java
@@ -2,13 +2,24 @@ package com.klst.einvoice.ubl;
 
 import oasis.names.specification.ubl.schema.xsd.commonbasiccomponents_2.IDType;
 
-public class Identifier {
+public class Identifier extends IDType {
 
+	@Deprecated
 	static IDType newIDType(String value, String schemeID) {
 		IDType ID = new IDType();
 		ID.setValue(value);
 		ID.setSchemeID(schemeID);
 		return ID;
+	}
+
+	Identifier(String value, String schemeID) {
+		super();
+		setValue(value);
+		setSchemeID(schemeID);
+	}
+	
+	Identifier(String value) {
+		this(value, null);
 	}
 
 }
