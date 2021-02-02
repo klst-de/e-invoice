02eff87661b9c38988168ed641590299bcaf0bc4
homebeaver <homebeaver@outlook.de>
2021-02-02 15:00:48 +0100

    doc

--- src/main/java/com/klst/einvoice/ITaxCategory.java
+++ src/main/java/com/klst/einvoice/ITaxCategory.java
@@ -7,26 +7,30 @@ import com.klst.untdid.codelist.TaxCategoryCode;
 /**
  * used in
  * <br> AllowancesAndCharges, DOCUMENT LEVEL ALLOWANCES (BG-20) and CHARGES (BG-21)
- * <br> BG-23 VAT BREAKDOWN
- * <br> BG-30 LINE VAT INFORMATION
+ * <br> BG-23 VAT BREAKDOWN : BT-118 , BT-119
+ * <br> BG-30 LINE VAT INFORMATION : BT-151
  */
 public interface ITaxCategory extends ITaxType {
 	
 	// ALLOWANCES (BG-20) and CHARGES (BG-21) BT-95-0, BT-102-0
-	// BG-23.BT-118-0
+	// VAT BREAKDOWN BG-23.BT-118-0
+	// LINE VAT INFORMATION : BG-30.BT-151-0
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setTaxType(String type);
 	@Override
 	public String getTaxType();
 	
 	// ALLOWANCES (BG-20) and CHARGES (BG-21) BT-95, BT-102 (mandatory) Document level allowance/charge VAT category code
-	// BG-23.BT-118
+	// VAT BREAKDOWN BG-23.BT-118
 	public void setTaxCategoryCode(String code);
 	public void setTaxCategoryCode(TaxCategoryCode code);
 	public TaxCategoryCode getTaxCategoryCode();
 	
 	// ALLOWANCES (BG-20) and CHARGES (BG-21) BT-96, BT-103 0..1 Document level allowance/charge VAT rate
-	// BG-23.BT-119
+	// VAT BREAKDOWN BG-23.BT-119
 	public void setTaxPercentage(BigDecimal percentage);
 	public BigDecimal getTaxPercentage();
 	
--- src/main/java/com/klst/einvoice/ITaxType.java
+++ src/main/java/com/klst/einvoice/ITaxType.java
@@ -1,6 +1,18 @@
 package com.klst.einvoice;
 
 /*
+Tax Categoty TypeCode : Code der Umsatzsteuerkategorie
+. CII Datentyp: qdt:TaxTypeCodeType
+. Beschreibung: Codierte Bezeichnung einer Umsatzsteuerkategorie
+. Hinweis: Fester Wert = "VAT"
+. Kardinalität: 1 .. 1
+. EN16931-ID: BT-118-0
+
+Codeliste: UNTDID 5153 Eingeschränkt
+In der EN 16931 wird nur die Steuerart „Umsatzsteuer“ mit dem Code „VAT“ unterstützt. 
+Sollen andere Steuerarten angegeben wie beispielsweise eine Versicherungssteuer oder eine Mineralölsteuer werden, 
+muss das EXTENDED Profil genutzt werden. 
+Der Code für die Steuerart muss dann der Codeliste UNTDID 5153 entnommen werden.
 
 UBL:
       <cac:TaxScheme>
@@ -11,13 +23,13 @@ UBL:
       <cbc:ID>E</cbc:ID>
       <cbc:Percent>0</cbc:Percent>
       <cac:TaxScheme>
-        <cbc:ID>VAT</cbc:ID>         <!-- TaxType -->
+        <cbc:ID>VAT</cbc:ID>         <!-- BT-118-0 Tax Categoty TypeCode -->
       </cac:TaxScheme>
     </cac:TaxCategory>
     
 CII: 
                 <ram:CategoryTradeTax>
-                    <ram:TypeCode>VAT</ram:TypeCode>                   <!-- TaxType -->
+                    <ram:TypeCode>VAT</ram:TypeCode>                   <!-- BT-118-0 Tax Categoty TypeCode -->
                     <ram:CategoryCode>E</ram:CategoryCode>
                     <ram:RateApplicablePercent>0</ram:RateApplicablePercent>
                 </ram:CategoryTradeTax>
@@ -25,6 +37,12 @@ CII:
  */
 public interface ITaxType {
 
+	/**
+	 * BG-23.BT-118-0 Tax Categoty TypeCode, 
+	 * also used in EN16931-IDs BG-20.BT-95-0 , BG-21.BT-102-0 and BG-30.BT-151-0
+	 * 
+	 * @param type typically VAT
+	 */
 	public void setTaxType(String type);
 	public String getTaxType();
 
