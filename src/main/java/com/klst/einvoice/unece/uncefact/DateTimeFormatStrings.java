95958e26c6c25a0aa06ec80ac6f004272cc3f0e6
homebeaver <homebeaver@outlook.de>
2021-02-05 16:26:29 +0100

    DateType toDate(Timestamp ts) used in TradeTax

--- src/main/java/com/klst/einvoice/unece/uncefact/DateTimeFormatStrings.java
+++ src/main/java/com/klst/einvoice/unece/uncefact/DateTimeFormatStrings.java
@@ -6,6 +6,7 @@ import com.klst.untdid.codelist.DateTimeFormats;
 
 import un.unece.uncefact.data.standard.qualifieddatatype._100.FormattedDateTimeType;
 import un.unece.uncefact.data.standard.unqualifieddatatype._100.DateTimeType;
+import un.unece.uncefact.data.standard.unqualifieddatatype._100.DateType;
 
 // in CII werden mehrere ähnliche Klassen verwendet, die Date(Time)String als inner Klasse haben
 // - DateType.DateString
@@ -13,17 +14,17 @@ import un.unece.uncefact.data.standard.unqualifieddatatype._100.DateTimeType;
 // - FormattedDateTimeType.DateTimeString
 public class DateTimeFormatStrings extends DateTimeFormats {
 
-//	static DateType toDate(Timestamp ts) {
-//		if(ts==null) return null;
-//		
-//		DateType.DateString ds = new DateType.DateString();
-//		ds.setFormat(DateTimeFormats.CCYYMMDD_QUALIFIER);
-//		ds.setValue(DateTimeFormats.tsToCCYYMMDD(ts));
-//		
-//		DateType date = new DateType();
-//		date.setDateString(ds);
-//		return date;
-//	}
+	static DateType toDate(Timestamp ts) {
+		if(ts==null) return null;
+		
+		DateType.DateString ds = new DateType.DateString();
+		ds.setFormat(DateTimeFormats.CCYYMMDD_QUALIFIER);
+		ds.setValue(DateTimeFormats.tsToCCYYMMDD(ts));
+		
+		DateType date = new DateType();
+		date.setDateString(ds);
+		return date;
+	}
 
 	static DateTimeType toDateTime(Timestamp ts) {
 		if(ts==null) return null;
